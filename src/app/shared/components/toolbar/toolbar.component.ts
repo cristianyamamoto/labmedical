1a9c0e3ce0aa2247718f61c2c1a8a5675b2f4fa5
Cristian Yamamoto <yamamotocristian@gmail.com>
2024-04-21 16:59:30 -0300

    feat: create toolbar

---
+++ src/app/shared/components/toolbar/toolbar.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.scss'
+})
+export class ToolbarComponent {
+  @Input() loggedUser: any;
+  @Input() usersList: any[] = [];
+
+  constructor(
+    private router: Router,
+    private authService: AuthService
+    ) {
+    this.authService.getLoggedUser.subscribe(user => this.loggedUser = user);
+  };
+
+  signOut(){
+    if(this.loggedUser){
+      this.loggedUser.auth = false;
+      localStorage.setItem("users", JSON.stringify(this.usersList));
+      this.router.navigate(["/login"]);
+      this.loggedUser = undefined;
+    }
+  }
+
+}
